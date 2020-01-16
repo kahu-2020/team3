653f76e30c91f48ca3e5e31a5d647a140b6107e8
claire-russell <russell.claireolivia@gmail.com>
2020-01-16 19:46:04 +1300

    animal blurb, image etc

---
+++ animal.js
@@ -0,0 +1,5 @@
+animal = {
+    'type': 'Gunnison sage grouse', 
+    'image': 'https://nc.iucnredlist.org/redlist/species-of-the-day/centrocercus-minimus/images/full/centrocercus-minimus.jpg' , 
+    'blurb': 'Your passionate, bold and eaily agitated. Your goal in life is to find prudent courtship, and always be just a touch too much.'
+}
