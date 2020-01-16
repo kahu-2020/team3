5dbb75d3f5e8e9910a799fddc430e31f3f70b098
Student <student@eda-bc-01.localdomain>
2020-01-17 10:30:28 +1300

    form input working

--- server.js
+++ server.js
@@ -8,8 +8,27 @@ server.engine('hbs', hbs({
   defaultLayout: 'main',
   extname: 'hbs'
 }))
+
+server.set('view options', {layout: 'main'})
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({extended: false}))
+
+//Global Variables
+let userName = '';
+
+
+server.get('/', (req, res) => {
+  res.render("home.hbs")
+})
+
+server.get('/1', (req, res) => {
+  res.render("quiz.hbs")
+})
+
+server.post('/name', (req, res) => {
+  username = req.body.name 
+  res.redirect('/1')
+})
 
 module.exports = server
