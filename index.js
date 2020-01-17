ab9c9ef263b1784ff478c50fc82a63ed00d1c1ab
Joshua-Simon <joshy.d.simon@gmail.com>
2020-01-17 11:52:45 +1100

    building blurb route

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ server.listen(port, function () {
   // eslint-disable-next-line no-console
   console.log('Server is listening on port', port)
 })
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const hbs = require('express-handlebars')
-
+const router = require('./router')
 const server = express()
 
 // Middleware
